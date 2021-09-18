99e30681d2181629f283db0109f60aff552f4d83
Simon <simon1240.se@gmail.com>
2021-09-18 17:09:51 +0200

    Fixed Name detection Prepared Icons Sorted Imports

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from io import DEFAULT_BUFFER_SIZE
-import pypxl
-import time
-import requests
-import threading
 import datetime
-import matplotlib.pyplot as plt
 import pathlib
 import sqlite3
+import threading
+import time
+from io import DEFAULT_BUFFER_SIZE
+
+import matplotlib.pyplot as plt
+import requests
+
+import pypxl
 
 webhook_onoff = "https://discord.com/api/webhooks/883775606540632075/hHrdNa-UHAaerqtoBWMdnenBsi0Tfd1-zsW78hPEIenvHNN1EA8IvEiNGvanko7zqiL_"
 webhook_mvp = "https://discord.com/api/webhooks/835654940008382464/RsN3Jjg8B6Ukv-8C09MfjktvyGrQztO4At2RIf27w4ZwmLpq_olf7kjr_YXPyAE8Cv43"
@@ -14,6 +16,8 @@ webhook_global = "https://discord.com/api/webhooks/835654823784874015/yXIkpU5K7m
 webhook_stats = "https://discord.com/api/webhooks/883773760447066112/qzeDM4A882s1DmvM7OXyswue_fQCnZL-F2xDu-iIyk5mB7CAN7ZmjJj1Gspz-ThQ5ezS"
 webhook_mods = "https://discord.com/api/webhooks/883807042656157828/053ufcOenaZo0dZHqBhz1Fd47SAt4qQ5_Wd3ZIMPo_RRcIGbBqguw1zjULrsS2QCMyJ0"
 webhook_mutes = "https://discord.com/api/webhooks/888503958904119356/t-v4e44YADLH7x5mF68XSj0nYKcV07dDRGFQ7z6fmkPTQnqBZ6bIAlfoECU_1Z7sjkOc"
+
+iconlist = {"_1_month" : "<:1month:883780503583465532>","_1_year" : "<:1year:883780503369568277>","_3_months" : "<:3months:883780503440871465>","_admin" : "<:admin:883780503323430933>","_booster" : "<:booster:883780503596060712>","_ppbread" : "<:ppbread:883780503713488916>","_chatmoderator" : "<:chatmoderator:883780503386357781>","_gifter" : "<:gifter:883780503646392370>","_moderator" : "<:moderator:883780503566704710>","_nitro" : "<:nitro:883780503675764736>","_paintingmoderator" : "<:paintingmoderator:883780503151460373>","_paintingowner" : "<:paintingowner:883780503780601866>","_partner" : "<:partner:883780503558299658>","_vip" : "<:vip:883780503516377108>"}
 
 global start_time
 start_time = datetime.datetime.utcnow()
@@ -61,6 +65,7 @@ def logChat7(data):
     if message == "":
         message = "/here"
     messageIcons = data["icons"]
+    messageIcons = messageIcons.replace("'",'"')
     messageChannel = data["channel"]
     messageMention = data["mention"]
     if message == "!restart":
@@ -84,6 +89,7 @@ def logChat7(data):
             Mentioned People:{messageMention}
             """
         timestamp = getTimeStamp()
+        iconstring = getIcons(messageIcons)
         content2 = f"Logged <t:{timestamp}:R>"
         timestamp = getTimeStamp()
         embed = {"description": f"{content}","title": "/7 Chat Message"}
@@ -104,6 +110,7 @@ def logChat8(data):
     if message == "":
         message = "/here"
     messageIcons = data["icons"]
+    messageIcons = messageIcons.replace("'",'"')
     messageChannel = data["channel"]
     messageMention = data["mention"]
     if messageChannel == "painting":
@@ -119,6 +126,7 @@ def logChat8(data):
             Mentioned People:{messageMention}
             """
         timestamp = getTimeStamp()
+        iconsstr = getIcons(icons)
         content2 = f"Logged <t:{timestamp}:R>"
         embed = {"description": f"{content}","title": "/8 Chat Message"}
         whdata = {"content": f"{content2}","username": f"{messageUsername} - {messageGuild}","embeds": [embed],}
@@ -184,14 +192,14 @@ def postJoins(data):
         embed = {"description": f"{content}","title": "Joins", "color": 2531122} #green
         whdata = {"content": f"Logged <t:{timestamp}:R>","username": "Joins","embeds": [embed],}
         postWebhook(webhook_onoff, whdata)
-        #This may or may not work.
         file = open(f"{CurrentDir}/banned.txt",'r')
         bannedlist = file.read().splitlines()
-        if data.lower() in str(bannedlist):
-            timestamp = getTimeStamp()
-            embed = {"description": f"Logged <t:{timestamp}:R>","title": "Permabanned User Detected!", "fields" : [{"name" : "Username", "value" : f"{data}"}], "color": 13571349} #red
-            whdata = {"content": "<@&835970992819273748>","username": "AutoMod","embeds": [embed],}
-            postWebhook(webhook_mods, whdata)
+        for name in bannedlist:
+            if str(name) in data.lower():
+                timestamp = getTimeStamp()
+                embed = {"description": f"Logged <t:{timestamp}:R>","title": "Permabanned User Detected!", "fields" : [{"name" : "Username", "value" : f"{data}"}], "color": 13571349} #red
+                whdata = {"content": "<@&835970992819273748>","username": "AutoMod","embeds": [embed],}
+                postWebhook(webhook_mods, whdata)
 
 @bot7.socketconnection.on("l")
 @background
@@ -211,7 +219,7 @@ def postMutes(data):
     whdata = {"content": "","username": "Chat Mutes","embeds": [embed],}
     postWebhook(webhook_mutes, whdata)      
 
-#check at a later point https://forms.gle/Ti9BoJEmDvzVGnwq7
+#To collect slurs: https://forms.gle/Ti9BoJEmDvzVGnwq7
 def checkChatMessage(message,username):
     spltmessage = message.split()
     file = open(f"{CurrentDir}/filter.txt",'r')
@@ -250,7 +258,17 @@ def getTimeStamp():
 	epoch = str(time.time()).split(".")[0]
 	return epoch
 
-
+def getIcons(icons):
+	iconsstr = ""
+	for icon in icons:
+		icon = icon.replace("-", "_")
+		icon = f"_{icon}"
+		if icon == "_bread":
+			icon = "_ppbread"
+		formatted_icon = iconlist[f"{icon}"]
+		iconsstr = f"{iconsstr}{formatted_icon}"
+	return iconsstr
+        
 def postWebhook(url, data):
     result = requests.post(url, json=data)
     if 200 <= result.status_code < 300:
@@ -269,5 +287,3 @@ if __name__ == "__main__":
 #Add check if data is same for leave/join
 #use sql lite thing
 #pray icons work
-#MaKe EmBeDs PrEtTy
-#user icons
