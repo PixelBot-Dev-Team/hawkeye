27637677c525c7a3855f03f1fcff7ad1ec0aeccd
JustCallMeSimon <github@jcms.dev>
2024-04-01 15:38:47 +0200

    ai is the best

--- lib/alertutil.py
+++ lib/alertutil.py
@@ -5,130 +5,151 @@ from lib.util import getTimeStamp
 
 # Function to create the database
 def create_database():
-    conn,cursor = getCandC()
+	conn,cursor = getCandC()
 
-    # pendingRedemptions table
-    cursor.execute("""--sql
-        CREATE TABLE IF NOT EXISTS pendingRedemptions (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT,
-            duration INTEGER
-        )
-    """)
+	# pendingRedemptions table
+	cursor.execute("""--sql
+		CREATE TABLE IF NOT EXISTS pendingRedemptions (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			username TEXT,
+			duration INTEGER
+		)
+	""")
 
-    # join alerts table
-    cursor.execute("""--sql
-        CREATE TABLE IF NOT EXISTS joinAlerts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            timestamp INTEGER,
-            username TEXT,
-            validUntil INTEGER,
-            discordId TEXT
-        )
-    """)
-    
-    # leave alerts table
-    cursor.execute("""--sql
-        CREATE TABLE IF NOT EXISTS leaveAlerts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            timestamp INTEGER,
-            username TEXT,
-            validUntil INTEGER,
-            discordId TEXT
-        )
-    """)
-    
-    # Custom word alerts table
-    cursor.execute("""--sql
-        CREATE TABLE IF NOT EXISTS customWordAlerts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            timestamp INTEGER,
-            messageText TEXT,
-            validUntil INTEGER,
-            discordId TEXT
-        )
-    """)
-    
-    conn.commit()
-    conn.close()
+	# join alerts table
+	cursor.execute("""--sql
+		CREATE TABLE IF NOT EXISTS joinAlerts (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			timestamp INTEGER,
+			username TEXT,
+			validUntil INTEGER,
+			discordId TEXT
+		)
+	""")
+	
+	# leave alerts table
+	cursor.execute("""--sql
+		CREATE TABLE IF NOT EXISTS leaveAlerts (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			timestamp INTEGER,
+			username TEXT,
+			validUntil INTEGER,
+			discordId TEXT
+		)
+	""")
+	
+	# Custom word alerts table
+	cursor.execute("""--sql
+		CREATE TABLE IF NOT EXISTS customWordAlerts (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			timestamp INTEGER,
+			messageText TEXT,
+			validUntil INTEGER,
+			discordId TEXT
+		)
+	""")
+	
+	conn.commit()
+	conn.close()
 
 def add_to_pending_redemptions(username,duration):
-    conn,cursor = getCandC()
+	conn,cursor = getCandC()
 
-    remove_from_pending_redemptions(username)
+	remove_from_pending_redemptions(username)
 
-    cursor.execute("""--sql
-        INSERT INTO pendingRedemptions (username, duration)
-        VALUES (?, ?)
-    """, (username, duration,))
+	cursor.execute("""--sql
+		INSERT INTO pendingRedemptions (username, duration)
+		VALUES (?, ?)
+	""", (username, duration,))
 
-    conn.commit()
-    conn.close()
+	conn.commit()
+	conn.close()
 
 def remove_from_pending_redemptions(username):
-    conn,cursor = getCandC()
-    
-    cursor.execute("""--sql
-        -- # TODO | remove all entries from pendingRedemptions where username is username
-    """)
+	conn,cursor = getCandC()
+	
+	cursor.execute("""--sql
+		-- # TODO | remove all entries from pendingRedemptions where username is username
+	""")
 
 def get_pending_redemptions(username):
-    conn,cursor = getCandC()
-    cursor.execute("""--sql
-                   SELECT duration FROM pendingRedemptions WHERE username = ?
-                   """, (username,))
-    data = cursor.fetchone()
-    return data if data is None else data[0] 
+	conn,cursor = getCandC()
+	cursor.execute("""--sql
+				   SELECT duration FROM pendingRedemptions WHERE username = ?
+				   """, (username,))
+	data = cursor.fetchone()
+	return data if data is None else data[0] 
 
 def add_join_alert(joiningUsername,duration,discordId):
-    conn,cursor = getCandC()
+	conn,cursor = getCandC()
 
-    timestamp = getTimeStamp()
+	timestamp = getTimeStamp()
 
-    cursor.execute("""--sql
-        INSERT INTO joinAlerts (timestamp, username, validUntil, discordId)
-        VALUES (?, ?, ?, ?)
-    """, (timestamp, joiningUsername, duration + timestamp, discordId))
+	cursor.execute("""--sql
+		INSERT INTO joinAlerts (timestamp, username, validUntil, discordId)
+		VALUES (?, ?, ?, ?)
+	""", (timestamp, joiningUsername, duration + timestamp, discordId))
 
-    conn.commit()
-    conn.close()
-    
+	conn.commit()
+	conn.close()
+	
 def get_active_join_alerts(username):
-    return "List of discordIds"
-    
+	conn, cursor = getCandC()
+	timestamp = getTimeStamp()
+	cursor.execute("""--sql
+		SELECT messageText, discordId FROM customWordAlerts WHERE validUntil > ?
+	""", (timestamp,))
+	data = cursor.fetchall()
+	conn.close()
+	return data
+	
 def add_leave_alert(leavingUsername,duration,discordId):
-    conn,cursor = getCandC()
+	conn,cursor = getCandC()
 
-    timestamp = getTimeStamp()
+	timestamp = getTimeStamp()
 
-    cursor.execute("""--sql
-        INSERT INTO leaveAlerts (timestamp, username, validUntil, discordId)
-        VALUES (?, ?, ?, ?)
-    """, (timestamp, leavingUsername, duration + timestamp, discordId))
+	cursor.execute("""--sql
+		INSERT INTO leaveAlerts (timestamp, username, validUntil, discordId)
+		VALUES (?, ?, ?, ?)
+	""", (timestamp, leavingUsername, duration + timestamp, discordId))
 
-    conn.commit()
-    conn.close()
-    
+	conn.commit()
+	conn.close()
+	
 def get_active_leave_alerts(username) -> Literal['List of discordIds']:
-    return "List of discordIds"
-    
+	conn, cursor = getCandC()
+	timestamp = getTimeStamp()
+	cursor.execute("""--sql
+		SELECT discordId FROM leaveAlerts WHERE username = ? AND validUntil > ?
+	""", (username, timestamp))
+	data = cursor.fetchall()
+	conn.close()
+	return [item[0] for item in data]    
+
 def add_customMessage_alert(text,duration,discordId):
-    conn,cursor = getCandC()
+	conn,cursor = getCandC()
 
-    timestamp = getTimeStamp()
+	timestamp = getTimeStamp()
 
-    cursor.execute("""--sql
-        INSERT INTO customWordAlerts (timestamp, messageText, validUntil, discordId)
-        VALUES (?, ?, ?)
-    """, (timestamp, text, duration + timestamp, discordId))
+	cursor.execute("""--sql
+		INSERT INTO customWordAlerts (timestamp, messageText, validUntil, discordId)
+		VALUES (?, ?, ?)
+	""", (timestamp, text, duration + timestamp, discordId))
 
-    conn.commit()
-    conn.close()
+	conn.commit()
+	conn.close()
 
 def get_active_custom_word_alerts():
-    return "list of customText,discordId"
+	conn, cursor = getCandC()
+	timestamp = getTimeStamp()
+	cursor.execute("""--sql
+		SELECT messageText, discordId FROM customWordAlerts WHERE validUntil > ?
+	""", (timestamp,))
+	data = cursor.fetchall()
+	conn.close()
+	return data
 
 def getCandC():
-    conn = sqlite3.connect('data/hawkeye_alerts.db')
-    cursor = conn.cursor()
-    return conn,cursor
+	conn = sqlite3.connect('data/hawkeye_alerts.db')
+	cursor = conn.cursor()
+	return conn,cursor
--- main.py
+++ main.py
@@ -38,49 +38,49 @@ while not connected:
 		print("didnt connect retrying")
 		time.sleep(2)
 
-startTime = datetime.datetime.utcnow()
+# startTime = datetime.datetime.utcnow()
 
-GlobalLog = ChatLogger(startTime,7,WH_DICT["WH_GLOBAL_URL"],WH_DICT["WH_OWMINCE_URL"],WH_DICT["WH_MUTE_URL"],WH_DICT["WH_ALERT_URL"],WH_DICT["WH_MOD_URL"])
-NonEngLog = ChatLogger(startTime,-1,WH_DICT["WH_NONENG_URL"],WH_DICT["WH_OWMINCE_URL"],WH_DICT["WH_MUTE_URL"],WH_DICT["WH_ALERT_URL"],WH_DICT["WH_MOD_URL"])
-AnarchyLog = ChatLogger(startTime,13,WH_DICT["WH_ANARCH_URL"],WH_DICT["WH_OWMINCE_URL"],WH_DICT["WH_MUTE_URL"],WH_DICT["WH_ALERT_URL"],WH_DICT["WH_MOD_URL"])
-MVPLog = ChatLogger(startTime,8,WH_DICT["WH_MVP_URL"],WH_DICT["WH_OWMINCE_URL"],WH_DICT["WH_MUTE_URL"],WH_DICT["WH_ALERT_URL"],WH_DICT["WH_MOD_URL"])
+# GlobalLog = ChatLogger(startTime,7,WH_DICT["WH_GLOBAL_URL"],WH_DICT["WH_OWMINCE_URL"],WH_DICT["WH_MUTE_URL"],WH_DICT["WH_ALERT_URL"],WH_DICT["WH_MOD_URL"])
+# NonEngLog = ChatLogger(startTime,-1,WH_DICT["WH_NONENG_URL"],WH_DICT["WH_OWMINCE_URL"],WH_DICT["WH_MUTE_URL"],WH_DICT["WH_ALERT_URL"],WH_DICT["WH_MOD_URL"])
+# AnarchyLog = ChatLogger(startTime,13,WH_DICT["WH_ANARCH_URL"],WH_DICT["WH_OWMINCE_URL"],WH_DICT["WH_MUTE_URL"],WH_DICT["WH_ALERT_URL"],WH_DICT["WH_MOD_URL"])
+# MVPLog = ChatLogger(startTime,8,WH_DICT["WH_MVP_URL"],WH_DICT["WH_OWMINCE_URL"],WH_DICT["WH_MUTE_URL"],WH_DICT["WH_ALERT_URL"],WH_DICT["WH_MOD_URL"])
 
 # Logging stuff into the db
-Datalogger = DataLogger(masterConnection)
+# Datalogger = DataLogger(masterConnection)
 
 # Chat Stats, Mutes, Announcements, Alerts, Join Leave, 
-MiscLogger = MiscLogger(masterConnection,WH_DICT["WH_MUTE_URL"],WH_DICT["WH_ANNOUNCE_URL"],WH_DICT["WH_ALERT_URL"],WH_DICT["WH_ONOFF_URL"],WH_DICT["WH_STATS_URL"])
+# MiscLogger = MiscLogger(masterConnection,WH_DICT["WH_MUTE_URL"],WH_DICT["WH_ANNOUNCE_URL"],WH_DICT["WH_ALERT_URL"],WH_DICT["WH_ONOFF_URL"],WH_DICT["WH_STATS_URL"])
 
 # Item Use, Item Gift
-ItemLogger = ItemLogger(masterConnection,WH_DICT["WH_GIFT_URL"],WH_DICT["WH_ITEMUSE_URL"])
+# ItemLogger = ItemLogger(masterConnection,WH_DICT["WH_GIFT_URL"],WH_DICT["WH_ITEMUSE_URL"])
 
 # Coins Island owner change
-CoinIslandLogger = CoinIslandLogger(masterConnection, WH_DICT["WH_CICHANGE_URL"])
+# CoinIslandLogger = CoinIslandLogger(masterConnection, WH_DICT["WH_CICHANGE_URL"])
 
 # war start and end
-WarLogger = WarLogger(masterConnection,WH_DICT["WH_WAR_URL"])
+# WarLogger = WarLogger(masterConnection,WH_DICT["WH_WAR_URL"])
 
 # New Auction/bid, ending auction
-AuctionLogger = AuctionLogger(masterConnection,WH_DICT["WH_AUCTION_URL"])
+# AuctionLogger = AuctionLogger(masterConnection,WH_DICT["WH_AUCTION_URL"])
 
 # Pixelplace code updates
-UpdateLogger = UpdateLogger(WH_DICT["WH_UPDATE_URL"])
+# UpdateLogger = UpdateLogger(WH_DICT["WH_UPDATE_URL"])
 
 # Custom Alerts
 CustomAlertLogger = CustomAlertLogger(masterConnection,WH_DICT["WH_CUSTOMALERT_URL"])
 
 # Owmince twitch chat
-TwitchLogger = TwitchLogger("owmince",WH_DICT["WH_TWITCH_URL"])
+# TwitchLogger = TwitchLogger("owmince",WH_DICT["WH_TWITCH_URL"])
 
 # ---
 
-from lib.util import getTimeStamp, postWebhook
-embed = {
-	"title": "Event",
-	"description": "Started!",
-	}
-whdata = {"content": f"Logged <t:{getTimeStamp()}:R>","username": "HawkEye (Event Logs)","embeds": [embed],}
-postWebhook(WH_DICT["WH_EVENT_URL"], whdata)
+# from lib.util import getTimeStamp, postWebhook
+# embed = {
+# 	"title": "Event",
+# 	"description": "Started!",
+# 	}
+# whdata = {"content": f"Logged <t:{getTimeStamp()}:R>","username": "HawkEye (Event Logs)","embeds": [embed],}
+# postWebhook(WH_DICT["WH_EVENT_URL"], whdata)
 
 while connected:
 	time.sleep(10)
